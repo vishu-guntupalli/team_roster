669110e8708e8cdf64690db68f91cd2565e34351
Vishu, Guntupalli <iamno.16@gmail.com>
2019-05-14 20:43:18 -0500

    Adding the hello button functionality

--- assets/roster.js
+++ assets/roster.js
@@ -1,29 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    document.getElementById("submitButton").onclick = function(){
+    var addedStudents = [];
+
+    document.getElementById("submitButton").onclick = function () {
         var studName = document.getElementById('rosterInput').value;
-        console.log(studName);
-        
-        var newStudentLine = $("<li>");
-        newStudentLine.text(studName);
-        $('#rosterList').append(newStudentLine);
-        newStudentLine.addClass("list-group-item");
+        if ((addedStudents.indexOf(studName) === -1)) {
+            console.log(studName);
 
-        var studentRemoveButton = $("<button>");
-        studentRemoveButton.text("Remove");
-        studentRemoveButton.addClass("btn btn-danger");
-        studentRemoveButton.attr("style","float:right");
-        studentRemoveButton.attr("id","removeButton");
-        newStudentLine.append(studentRemoveButton);
+            var newStudentLine = $("<li>");
+            newStudentLine.text(studName);
+            $('#rosterList').append(newStudentLine);
+            newStudentLine.addClass("list-group-item " + studName);
 
-        $("#rosterInput").val("");
+            var studentRemoveButton = $("<button>");
+            studentRemoveButton.text("Remove");
+            studentRemoveButton.addClass("btn btn-danger removeButton");
+            studentRemoveButton.attr("style", "float:right");
+            studentRemoveButton.attr("studName", studName);
+            newStudentLine.append(studentRemoveButton);
 
-        $("#removeButton").on('click', '', function(){
-            newStudentLine.remove();
+            $("#rosterInput").val("");
+
+            addedStudents.push(studName);
+        }
+        else {
+            alert("This student is already present");
+        }
+
+        $(".removeButton").on('click', function () {
+            var sureToDelete = confirm("Do you really want to delete the " + studName + " ?");
+            if (sureToDelete) {
+                $('.' + $(this).attr('studName')).remove();
+                addedStudents.splice(addedStudents.indexOf(this.studName), 1);
+            }
         });
+
     }
 
-   
-
+    $("#helloButton").on('click',function(){
+        for(var i=0; i<addedStudents.length; i++) {
+            alert("Hello "+addedStudents[i]);
+        }
+    });
 
 });
